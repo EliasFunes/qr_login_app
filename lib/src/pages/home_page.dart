7c71a15c1711945a7c483df6b4999643a75313c5
Elias Funes <eliasfunesriveros@gmail.com>
2023-06-20 18:18:20 -0400

    Se borra page para scanner y se agrega el boton de scanner en el home, directo.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show json, base64, ascii;
 
-import 'package:qr_login_app/src/pages/scanner_page.dart';
-
-class HomePage extends StatelessWidget {
-  HomePage(this.jwt, this.payload, this.portAndHost);
+class HomePage extends StatefulWidget {
+  final String jwt;
+  final Map<String, dynamic> payload;
+  final String portAndHost;
 
   factory HomePage.fromBase64(String jwt, String portAndHost) => HomePage(
       jwt,
@@ -13,46 +14,37 @@ class HomePage extends StatelessWidget {
           ascii.decode(base64.decode(base64.normalize(jwt.split(".")[1])))),
       portAndHost);
 
-  final String jwt;
-  final Map<String, dynamic> payload;
-  final String portAndHost;
+  HomePage(this.jwt, this.payload, this.portAndHost);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    Map valueMap = json.decode(jwt);
+    Map valueMap = json.decode(widget.jwt);
     String token = valueMap['token'];
+    String ipPort = widget.portAndHost;
+
+    _scan() async => await FlutterBarcodeScanner.scanBarcode(
+                "#2560f5", "Cancel", true, ScanMode.QR)
+            .then((value) {
+          setState(() {
+            http.post(Uri.http(ipPort, 'ws/sendToUser'),
+                headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": "Bearer $token",
+                },
+                body: json.encode({"tokenQr": value}));
+          });
+        });
+
     return Scaffold(
       appBar: AppBar(title: Text("QR-Login-App")),
-      body: Center(
-        child: FutureBuilder(
-            future: http.read(Uri.http(portAndHost, 'test/jdbc_test'),
-                headers: {
-                  "Authorization": 'Bearer $token',
-                  'Content-Type': 'text/plain'
-                }),
-            builder: (context, snapshot) => snapshot.hasData
-                ? Column(
-                    children: <Widget>[
-                      Text("${payload['username']}, here's the data:"),
-                      Text(snapshot.data.toString(),
-                          style: Theme.of(context).textTheme.displayMedium)
-                    ],
-                  )
-                : snapshot.hasError
-                    ? Column(
-                        children: [
-                          Text("An error occurred ${snapshot.error}"),
-                          Text("portAndHost: $portAndHost, Bearer $token")
-                        ],
-                      )
-                    : CircularProgressIndicator()),
-      ),
+      body: Center(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-          var splited = portAndHost.split(":");
-          return ScannerPage(splited.first, splited[1], jwt);
-        })),
+        onPressed: () => _scan(),
         child: Icon(Icons.qr_code_scanner),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
